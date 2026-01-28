7d563b720a3be7f3da1f37183e7e45fddbf96172
박순홍 <hishp@pusan.ac.kr>
2026-01-28 20:04:57 +0900

    course3 라이다 전처리 보정 완성

--- isv/launch_isv/course3.py
+++ isv/launch_isv/course3.py
@@ -9,7 +9,7 @@ import os
 import yaml
 from math import degrees, atan2
 from sensor_msgs.msg import LaserScan, Imu
-from std_msgs.msg import Float64, String
+from std_msgs.msg import Float64
 from rclpy.qos import qos_profile_sensor_data
 from rclpy.node import Node
 from tf_transformations import euler_from_quaternion
@@ -27,13 +27,10 @@ class Course3(Node):
         self.key_publisher = self.create_publisher(Float64, "/actuator/key/degree", 10)
         self.thruster_publisher = self.create_publisher(Float64, "/actuator/thruster/percentage", 10)
         self.curr_yaw_publisher = self.create_publisher(Float64, "/current_yaw", 10)
-        self.safe_angle_list_publisher = self.create_publisher(String, "/safe_angles_list", 10)
         self.max_distance_publisher= self.create_publisher(Float64, "/max_distance", 10)
         self.best_angle_publisher = self.create_publisher(Float64,"/best_angle", 10)
         self.cmd_key_degree = self.servo_neutral_deg
         self.cmd_thruster = self.default_thrust
-        self.dist_threshold = 0.3 # 장애물로 인식할 거리 (m)
-        self.side_margin = 10 # 장애물로 처리할 좌우 각도
         self.imu_heading = 0.0
         self.stop_dist_m = 0.3
         self.initial_yaw_abs = None
@@ -71,9 +68,8 @@ class Course3(Node):
         ranges = np.array(data.ranges)
         start_idx, end_idx = 500, 1500
         subset = ranges[start_idx:end_idx]
-        dist_buckets = [[] for _ in range(181)] 
         dist_180 = np.zeros(181)
-
+        dist_buckets = [[] for _ in range(181)]
         for i in range(len(subset)):
             length = subset[i]
             if length <= data.range_min or length >= data.range_max or not np.isfinite(length):
@@ -83,36 +79,35 @@ class Course3(Node):
                 dist_buckets[angle_index].append(length)
         for j in range(181):
             if dist_buckets[j]:
-                dist_180[j] = np.median(dist_buckets[j])
+                rounded_lengths = [round(val * 20) / 20 for val in dist_buckets[j]]
+                counts = {}
+                for val in rounded_lengths:
+                    counts[val] = counts.get(val, 0) + 1
+                dist_180[j] = max(counts, key=counts.get)
             else:
                 dist_180[j] = 0.0
-        danger_flags = (dist_180 > 0) & (dist_180 <= self.dist_threshold)
-        expanded_danger = np.copy(danger_flags)
-        n = self.side_margin
-        for i in range(181):
-            if danger_flags[i]:
-                expanded_danger[max(0, i - n) : min(181, i + n + 1)] = True
-        self.safe_angles_list = [int(deg) for deg in range(181) if not expanded_danger[deg]]
-
-        if self.safe_angles_list is not None:
-            list_msg = String()
-            list_msg.data = str(self.safe_angles_list)
-            self.safe_angle_list_publisher.publish(list_msg)
-        safe_distances = [dist_180[deg] for deg in self.safe_angles_list]
-        relative_best_idx = np.argmax(safe_distances)
-        best_angle_idx = self.safe_angles_list[relative_best_idx]
-        max_distance = float(dist_180[best_angle_idx])
-
+        best_i = int(np.argmax(dist_180))
+        new_max_distance = float(dist_180[best_i])
+        if not hasattr(self, 'prev_max_dist'):
+            self.prev_max_dist = new_max_distance
+        if abs(new_max_distance - self.prev_max_dist) > 1.0:
+            max_distance = self.prev_max_dist
+        else:
+            max_distance = new_max_distance
+            self.prev_max_dist = new_max_distance
+        best_lidar_angle = float(-90 + best_i)
         self.max_distance_publisher.publish(Float64(data=max_distance))
-        self.best_angle = float(best_angle_idx)
-        self.best_angle_publisher.publish(Float64(data=self.best_angle))
-
+        self.best_angle = best_lidar_angle + 90.0
+        self.best_angle_publisher.publish(Float64(data=float(self.best_angle)))
         if max_distance <= self.stop_dist_m:
             self.key_publisher.publish(Float64(data=float(self.servo_neutral_deg)))
             self.thruster_publisher.publish(Float64(data=0.0))
             return
-
-        self.cmd_key_degree = constrain(self.best_angle, self.servo_min_deg, self.servo_max_deg)
+        self.cmd_key_degree = constrain(
+            self.best_angle, 
+            self.servo_min_deg, 
+            self.servo_max_deg
+        )
         self.key_publisher.publish(Float64(data=self.cmd_key_degree))
         self.thruster_publisher.publish(Float64(data=self.cmd_thruster))
 
